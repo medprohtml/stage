1e474100206172a4372f4e1ffb73b52b32e09e29
medprohtml <2001120500400@ofppt-edu.ma>
2024-02-27 18:35:48 +0100

    add

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,14 +7,17 @@
    @vite(['resources/js/app.js'])
 
 </head>
-<body class=" px-5">  
-    <nav id="nav" class="   md:pb-0 md:pt-0   w-full  rounded-md flex justify-between px-6 h-16 shadow-xl items-center bg-slate-50  font-medium text-sm">
+<body dir="rtl" class="  ">  
+  <div dir="ltr" class="  w-ful l flex justify-center text-center  text-lg font-bold gap-4 items-center h-20  text-blue-400 bg-white shadow-md rounded-md px-3 py-2"> <img class=" w-16 h-16 object-cover" src="https://lh3.googleusercontent.com/a-/ALV-UjWAHUTwybvDtmQb5zCza7ScTkmgbfVV3E34HcCqYQsNVw=w90-h90-p-rp-mo-br100" alt=""> <p>Centre d'Analyses Médicales Al Hayat </p></div>
+  <p class=" w-full text-center flex justify-center text-white text-sm font-medium py-3 px-3 bg-red-600">خدماتنا من الساعة 8 صباحًا حتى 6 مساءً ما عدا الأحد من 8 صباحًا حتى 5 مساءً.    <span class=" ms-7"> Nos service de 8h à 18h sauf samedi et dimanche de 8h à 16Hn</span> </p>
+
+    <nav dir="rtl" id="nav" class="   md:pb-0 md:pt-0   w-full  rounded-md flex justify-between px-6 h-16 shadow-xl items-center bg-slate-50  font-medium text-sm">
    
+      <h1 class="  logo font-extrabold text-lg text-green-500">CAMH </h1>
       <p id="btn" class=" absolute left-9 lg:hidden " ><svg class=" w-6 h-6" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 30 30">
         <path d="M 3 7 A 1.0001 1.0001 0 1 0 3 9 L 27 9 A 1.0001 1.0001 0 1 0 27 7 L 3 7 z M 3 14 A 1.0001 1.0001 0 1 0 3 16 L 27 16 A 1.0001 1.0001 0 1 0 27 14 L 3 14 z M 3 21 A 1.0001 1.0001 0 1 0 3 23 L 27 23 A 1.0001 1.0001 0 1 0 27 21 L 3 21 z"></path>
         </svg></p>
-      <p id="btn" class=" absolute right-9" >menu</p>
-      <ul id="menu" class=" delay-200  transition-all hidden nav mt-64 md:mt-0 flex flex-col md:flex-row gap-3   md:flex md:gap-16 ">
+      <ul dir="rtl" id="menu" class=" delay-200  transition-all hidden nav mt-64 md:mt-0 flex flex-col md:flex-row gap-3   md:flex md:gap-16 ">
 
         <li class=" px-5 py-3 text-white rounded-lg bg-green-600" ><a href="{{route('showpatinet')}}" >صفحة المسوول</a></li>
         <li class=" px-5 py-3 text-white rounded-lg bg-green-600"><a  href="{{route('loginshow')}}" > نتائج التحاليل</a></li>
